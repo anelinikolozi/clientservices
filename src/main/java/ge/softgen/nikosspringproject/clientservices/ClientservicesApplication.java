37c6e784632d2f51c6a35c3c1302f6543fbf1c56
nikolozianeli <nikolozianeli@gmail.com>
2021-10-20 11:47:21 +0400

    Initial commit

---
+++ src/main/java/ge/softgen/nikosspringproject/clientservices/ClientservicesApplication.java
@@ -0,0 +1,13 @@
+package ge.softgen.nikosspringproject.clientservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientservicesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClientservicesApplication.class, args);
+    }
+
+}
